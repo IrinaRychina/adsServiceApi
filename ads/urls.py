29ecc51ed7e4ff6f1605646db1ca5df0f4cd35a7
Rychina Irina <ry4ina.irina@yandex.ru>
2023-06-18 00:02:28 +0500

    added user authorisation pages and home page

--- ads/urls.py
+++ ads/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path
+from .views import home
 from .views import create_ad
 from .views import ad_list
+from .views import register
+from .views import user_login
+from .views import user_logout
 
 urlpatterns = [
+    path('', home, name='home'),
     path('ads/', ad_list, name='ad_list'),
-    path('create-ad/', create_ad, name='create_ad')
+    path('create-ad/', create_ad, name='create_ad'),
+    path('register/', register, name='register'),
+    path('login/', user_login, name='login'),
+    path('logout/', user_logout, name='logout'),
 ]
--- ads/views.py
+++ ads/views.py
@@ -1,10 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login, logout
 
 from .models import Ad
 from .forms import AdForm
 from notifications.utils.notifications import create_notification
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm
 
+def home(request):
+    return render(request, 'home.html')
 
 @login_required
 def create_ad(request):
@@ -27,3 +32,28 @@ def create_ad(request):
 def ad_list(request):
     ads = Ad.objects.all()
     return render(request, 'ads/ad_list.html', {'ads': ads})
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/register.html', {'form': form})
+
+def user_login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('/')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'registration/login.html', {'form': form})
+
+def user_logout(request):
+    logout(request)
+    return redirect('/')
