886d2de2cb3dd71fb35c65fea524c233e4f9bafb
Rychina Irina <ry4ina.irina@yandex.ru>
2023-07-08 22:51:29 +0500

    added proper validation on the registration form

--- ads/views.py
+++ ads/views.py
@@ -7,7 +7,7 @@ from django.conf import settings
 from .models import Ad
 from .forms import AdForm
 from notifications.utils.notifications import create_notification
-from django.contrib.auth.forms import UserCreationForm
+from .forms import RegistrationForm
 from django.contrib.auth.forms import AuthenticationForm
 
 def home(request):
@@ -61,12 +61,12 @@ def ad_list_authorized(request):
 
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = RegistrationForm(request.POST)
         if form.is_valid():
-            form.save()
+            user = form.save_user()
             return redirect('login')
     else:
-        form = UserCreationForm()
+        form = RegistrationForm()
     return render(request, 'registration/register.html', {'form': form})
 
 def user_login(request):
